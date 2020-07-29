a2896cae618711f3647a82cd15180399cb4ea26f
tajammal2 <68955031+tajammal2@users.noreply.github.com>
2020-07-29 17:59:15 +0500

    Urdu Language File phpml.lang-ur.php
    For Urdu language of Pakistan.

---
+++ language/phpml.lang-ur.php
@@ -0,0 +1,131 @@
+<?php
+/*~ phpml.lang-ur.php
+.---------------------------------------------------------------------------.
+|  Software: PHPMailer-ML - Urdu Language File                           |
+'---------------------------------------------------------------------------'
+
+ * @author Tajammal Hussain
+ */
+
+$PHPML_LANG = array();
+
+/* labels */
+$PHPML_LANG["add_new_list"]             = 'فہرست شامل کریں';
+$PHPML_LANG["added"]                    = 'شامل ہوگئی';
+$PHPML_LANG["adminBcc"]                 = 'ایڈمن بیک اپ کاپی';
+$PHPML_LANG["adminBcc_explain"]         = '<small>اگر نشان زد ہوگا تو سبسکرپشن پر ایڈمن کو کاپی بھیجی جائے گی</small>';
+$PHPML_LANG["appname"]                  = 'ایپلیکیشن نام';
+$PHPML_LANG["bcc"]                      = 'بیک اپ کاپی';
+$PHPML_LANG["campaign"]                 = 'مہم';
+$PHPML_LANG["campaigns"]                = 'مہمات';
+$PHPML_LANG["click_continue"]           = 'جاری رکھنے کیلئے یہاں کلک کریں۔۔۔';
+$PHPML_LANG["configuration"]            = 'اصلاحات';
+$PHPML_LANG["continue"]                 = 'جاری';
+$PHPML_LANG["comma"]                    = 'کوما/وقفہ';
+$PHPML_LANG["CSVheader"]                = 'سی۔ایس۔وی فائل میں ہیڈر رو شامل ہے';
+$PHPML_LANG["db_host"]                  = 'ڈیٹابیس ہوسٹ';
+$PHPML_LANG["db_name"]                  = 'ڈیٹابیس نام';
+$PHPML_LANG["db_user"]                  = 'ڈیٹابیس صارف نام';
+$PHPML_LANG["db_pass"]                  = 'ڈیٹابیس پاسورڈ';
+$PHPML_LANG["databases"]                = 'ڈیٹابیس';
+$PHPML_LANG["deleted"]                  = 'حذف کیا گیا';
+$PHPML_LANG["deleteselected"]           = 'منتخب شدہ حذف کریں';
+$PHPML_LANG["edit"]                     = 'ترمیم';
+$PHPML_LANG["english"]                  = 'انگلش';
+$PHPML_LANG["email_format"]             = 'ڈیزائن';
+$PHPML_LANG["email_htmltext"]           = 'ایچ۔ٹی۔ایم۔ایل &amp; متن';
+$PHPML_LANG["email_textonly"]           = 'صرف متن';
+$PHPML_LANG["error_uploading_file"]     = 'فائل اپلوڈ کرتے ہوئے مسئلہ';
+$PHPML_LANG["false"]                    = 'غلط';
+$PHPML_LANG["field_enclosure"]          = 'فیلڈ شمولیت کنندہ';
+$PHPML_LANG["field_separator"]          = 'فیلڈ علیحدگی کنندہ';
+$PHPML_LANG["file_not_writable"]        = 'یہ فائل لکھنے کے قبال نہیں کچھ نہیں ہوسکتا';
+$PHPML_LANG["format"]                   = 'فارمیٹ';
+$PHPML_LANG["from"]                     = 'بھیجنے والا';
+$PHPML_LANG["fromname"]                 = 'بھیجنے والے کا نام';
+$PHPML_LANG["fromaddy"]                 = 'بھیجنے والے کی ای۔میل';
+$PHPML_LANG["go"]                       = 'جائیں';
+$PHPML_LANG["import"]                   = 'سی۔ایس۔وی درآمد کریں۔';
+$PHPML_LANG["import_options"]           = 'فائل آپشنز امپورٹ کریں';
+$PHPML_LANG["list_records"]             = 'فہرست ریکارڈ';
+$PHPML_LANG["mailinglist"]              = 'میلنگ فہرست';
+$PHPML_LANG["mailinglists"]             = 'میلنگ فہرستیں';
+$PHPML_LANG["makedefault"]              = 'طے شدہ بنائیں';
+$PHPML_LANG["language"]                 = 'زبان';
+$PHPML_LANG["list"]                     = 'فہرست';
+$PHPML_LANG["lists"]                    = 'فہرستیں';
+$PHPML_LANG["message"]                  = 'پیغام';
+$PHPML_LANG["name_email"]               = 'ای۔میل';
+$PHPML_LANG["name_first"]               = 'پہلا نام';
+$PHPML_LANG["name_last"]                = 'آخری نام';
+$PHPML_LANG["need_to_map"]              = 'ڈیٹابیس فیلڈز میں میپ رول کی ضرورت ہے';
+$PHPML_LANG["none"]                     = 'کچھ نہیں';
+$PHPML_LANG["or"]                       = 'یا';
+$PHPML_LANG["preferences"]              = 'ترجیحات';
+$PHPML_LANG["preview"]                  = 'منظرنامہ';
+$PHPML_LANG["quotedouble"]              = 'ڈبل کوٹس';
+$PHPML_LANG["quotesingle"]              = 'سنگل کوٹ';
+$PHPML_LANG["select"]                   = 'منتخب';
+$PHPML_LANG["send"]                     = 'بھجیں';
+$PHPML_LANG["sendemails"]               = 'ای۔میلز بھیجیں';
+$PHPML_LANG["sendemails_explain"]       = '<small>اگر منتخب ہوگا تو ای۔میل بھیجی جائیں گی اگر غیرمنتخب ہوگا تو ای۔میل بھیجنا بند ہوجائے گا</small>';
+$PHPML_LANG["sent"]                     = 'بھیجی گئی';
+$PHPML_LANG["settings"]                 = 'اصلاحات';
+$PHPML_LANG["settings_email"]           = 'بھیجی جانے والی ای۔میل کی ترتیبات';
+$PHPML_LANG["settings_db"]              = 'ڈیٹابیس کی ترتیبات';
+$PHPML_LANG["SMTPhost"]                 = 'SMTP ترتیبات';
+$PHPML_LANG["SMTPauth"]                 = 'SMTP توثیق';
+$PHPML_LANG["SMTPusername"]             = 'SMTP صارف نام';
+$PHPML_LANG["SMTPpassword"]             = 'SMTP پاسورڈ';
+$PHPML_LANG["SMTPport"]                 = 'SMTP پورٹ';
+$PHPML_LANG["save"]                     = 'محفوظ';
+$PHPML_LANG["subject"]                  = 'عنوان';
+$PHPML_LANG["submit"]                   = 'جمع کریں';
+$PHPML_LANG["subscriber_records"]       = 'سبسکرائبر ریکارڈز';
+$PHPML_LANG["subscriber"]               = 'سبسکرائبر';
+$PHPML_LANG["subscribers"]              = 'سبسکرائبرز';
+$PHPML_LANG["tab"]                      = 'ٹیب';
+$PHPML_LANG["to"]                       = 'منجانب';
+$PHPML_LANG["thankyou"]                 = 'آپ کا شکریہ!';
+$PHPML_LANG["true"]                     = 'صحیح';
+$PHPML_LANG["type"]                     = 'قسم';
+$PHPML_LANG["updated"]                  = 'تازہ ترین';
+$PHPML_LANG["updating"]                 = 'تازہ ہورہا ہے';
+$PHPML_LANG["uploadfile"]               = 'فائل اپلوڈ کریں';
+$PHPML_LANG["usebcc"]                   = 'بیک اپ کاپی استعمال کریں';
+$PHPML_LANG["usebcc_explain"]           = '<small>اگر منتخب ہوگا تو بیک اپ کاپی ای۔میل بھیجنے کے لیے استعمال ہوگی اگر غیر منتخب ہوگا تو صرف ای۔میل بھیجی جائیں گی۔ نوٹ: ای۔ایم۔ٹی۔پی بیک اپ کاپی ک ا استعمال نہیں کرے گا.</small>';
+$PHPML_LANG["which_list"]               = 'فہرست استعمال کریں';
+
+/* messages / phrases */
+$PHPML_LANG["lib_not_found"]            = 'مسئلہ: PHPMailer لائبریری نہیں ملی';
+                                          'آپ کے ایڈریس میں <i>یا</i> اس پر';
+$PHPML_LANG["csv_not_found"]            = 'مسئلہ: سی۔ایس۔وی فہرستوں والی کھولنے میں ناکامی.';
+$PHPML_LANG["no_email"]                 = 'مسئلہ: کوئی بھی ای۔میل ایڈریس نہیں لکھا گیا';
+$PHPML_LANG["no_display"]               = 'مسئلہ: دکھان کے لیے کچھ نہیں';
+$PHPML_LANG["error_sending"]            = 'مسئلہ: ای۔میل بھیجنے میں دشواری';
+$PHPML_LANG["error_query"]              = 'مسئلہ: کویری کو چلانے میں ناکام';
+$PHPML_LANG["error_unsubscribe"]        = 'مسئلہ: مہیا کیا گیا ان۔سبسکرائب لنک درست نہیں';
+$PHPML_LANG["success_sending"]          = 'پیغام کامیابی سے بھیجا گیا';
+$PHPML_LANG["send_another"]             = 'مزید پیغام بھیجنا چاہیں گے؟';
+$PHPML_LANG["compose_message"]          = 'نیا پیغام لکھیں';
+$PHPML_LANG["compose1"]                 = 'نیام پیغام لکھیں برائے';
+$PHPML_LANG["compose2"]                 = ' میلنگ فہرست';
+$PHPML_LANG["Confirmed"]                = 'ای۔میل ایڈریس کی توثیق ہوگئی ہے۔ آپ کا شکریہ!';
+$PHPML_LANG["not_subscribed"]           = ' سبسکرائب نہیں کیا';
+$PHPML_LANG["now_registered_to"]        = ' اب رجسٹر ہوگیا ہے برائے';
+$PHPML_LANG["now_registered"]           = ' اب رجسٹر ہوگیا ہے';
+$PHPML_LANG["now_subscribed_to"]        = ' اب سبسکرائب ہوگیا ہے برائے';
+$PHPML_LANG["now_subscribed"]           = ' اب سبسکرائب ہوگیا ہے';
+$PHPML_LANG["now_unsubscribed_from"]    = ' اب ان۔سبسکرائب ہوگیا ہے';
+$PHPML_LANG["selfsubscribe1"]           = 'یہ ای۔میل ایڈریس سبسکرائب کرنے کے لیے استعمال کیا گیا ہے برائے' .
+                                          $phpml['ListName'] . ' میلنگ فہرستیں۔ اگر آپ نے یہ درخواست کی ہے تو' .
+                                          'برائے مہربانی یہاں کلک کریں:' . "\n";
+$PHPML_LANG["selfsubscribe2"]           = "\n\n" . 'اگر آپ نے اس عمل کی درخواست نہیں کی تو برائے مہربانی ' .
+                                          'اس ای۔میل کو نظرانداز کریں۔ آپ مزید اطلاعات وصول نہیں کریں گے';
+$PHPML_LANG["subscribe"]                = 'سبسکرائب کرنے کے لیے یہاں کلک کریں';
+$PHPML_LANG["subscribe_confirm"]        = ' سبسکرپشن کی توثیق کے لیے آپ کو ای۔میل بھیجی جائے گی۔';
+$PHPML_LANG["already_subscribed"]       = ' پہلے ہی سبسکرائب کیا ہوا ہے';
+$PHPML_LANG["unsubscribe"]              = 'ان۔سبسکرائب کرنے کے لیے کلک کریں';
+$PHPML_LANG["unsubscribed"]             = ' ان۔سبسکرائب ہوگیا ہے';
+
+?>
